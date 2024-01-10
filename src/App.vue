29e174d18201c46cfbc638a8cd613acd9ec863ec
samim <s.dan.amoz@gmail.com>
2024-01-10 22:03:11 +0330

    base of vue

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <SiteNavigation/>
+<RouterView/>
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router';
+import SiteNavigation from './components/siteNavigation.vue';
+</script>
+
+<style lang="scss" scoped>
+
+</style> b 
---
+++ src/components/siteNavigation.vue
@@ -0,0 +1,14 @@
+<template>
+	<header class="sticky top-0 bg-weather-primary shadow-lg">
+		<nav
+			class="container flex flex-col items-center sm:flex-row gap-4 text-white py-6"
+		>
+			<div class="flex items-center flex-1 gap-3">
+				<i class="fa-solid fa-sun text-2xl"></i>
+				<p class="text-2xl">The Local Weather</p>
+			</div>
+		</nav>
+	</header>
+</template>
+
+<script setup></script>
