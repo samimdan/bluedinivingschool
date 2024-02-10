cf36cdce52f5955db8c2d891db8f7a87c7b37a1a
samimdan <107123726+samimdan@users.noreply.github.com>
2024-02-10 20:13:57 +0330

    class
    add migleardAnimation moudle

--- src/main.js
+++ src/main.js
@@ -6,19 +6,12 @@ import router from './router'
 import './assets/tailwind.css'
 import './assets/css/main.css'
 import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-const vuetify = createVuetify({
-    components,
-    directives,
 
-})
 
 
 
 const app = createApp(App)
 app.use(router)
-app.use(vuetify).mount('#app')
+app.mount('#app')
 
 
